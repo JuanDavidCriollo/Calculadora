c053b526296d3da3c8c2d29287b591472e2f98b0
Juan <juandavid.criollo@bghtechpartner.com>
2019-04-09 16:10:38 -0500

    Version 3 final con los js

--- funciones.js
+++ funciones.js
@@ -1,3 +1,16 @@
+// funcion que se ejeucta despues de clickear un boton de la clase boton en la cual se inserta en el campo de operacion
+function Insertar(){
+	document.getElementById("casilla").value = document.getElementById("casilla").value + this.value;
+}
+//funcion en lacual se evalua evalua el contenido de el campo de operacion y se escribira hay mismo la respuesta
+function Operar(){
+	document.getElementById("casilla").value = eval(document.getElementById("casilla").value);
+}
+//funcion para limpiar el campo de operacion
+function Limpiar(){
+	document.getElementById("casilla").value = "";
+}
+//funcion que se ejecutara cuando se cargue la ventana
 function Eventos(){
 	//en estos tres fragmentos de codigo se va a asignar los eventos onclick a los botones y su respectiva función
 	//evento on click asignados a todos los elementos que sean clase boton
@@ -12,3 +25,17 @@ function Eventos(){
 }
 //evento que direccionara a la funcion Eventos cuando secargue la ventana
 window.onload = Eventos;
+
+//funcion que escribe el texto en el lienzo canvas con su respctivo evento
+function baner(){
+	var elemento = document.getElementById('lienzo');
+	lienzo = elemento.getContext('2d');
+
+	lienzo.font = "bold 18px  verdana, sans-serif";
+	lienzo.textAlign = "center";
+	lienzo.textBaseLine= "top";
+
+	lienzo.strokeText("Calculadora ", 210, 25);
+}
+
+window.addEventListener("load",baner, false);
